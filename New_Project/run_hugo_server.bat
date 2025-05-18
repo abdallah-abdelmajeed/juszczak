dadd7051d20cef49fee4d2e48c0c716afcff45ee
Abdallah Abdelmajeed <69306956+abdallah-abdelmajeed@users.noreply.github.com>
2025-05-18 19:17:07 +0200

    new file:   New_Project/hugo.zip 	new file:   New_Project/hugo/LICENSE 	new file:   New_Project/hugo/README.md 	new file:   New_Project/hugo/hugo.exe 	new file:   New_Project/run_hugo_server.bat 	modified:   New_Project/hugo-site (untracked content)

---
+++ New_Project/run_hugo_server.bat
@@ -0,0 +1,5 @@
+@echo off
+SET PATH=%PATH%;C:\Program Files\Go\bin
+cd c:\Users\user\Downloads\Juszczak_page\New_Project\hugo-site
+C:\Users\user\Downloads\Juszczak_page\New_Project\hugo\hugo.exe server -D
+pause
